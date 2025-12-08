2fef72440ee3511f8a1bdb67bfa3953d96b881b1
Tomas van Rijsse <tomas@pionect.nl>
2025-12-08 10:52:49 +0100

    feat: add pwa auto update

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { registerSW } from 'virtual:pwa-register'
+
+// Register service worker for PWA updates
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // Auto-reload when new version is available
+    updateSW(true)
+  },
+  onOfflineReady() {
+    console.log('App ready to work offline')
+  },
+})
 
 createApp(App).mount('#app')
